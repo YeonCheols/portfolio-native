d6ef38a725f2dbe3a6ac76c428df8fb40b56ac93
SungStack <s9292909@gmail.com>
2025-06-25 08:34:48 +0900

    react-native 웹뷰 초기 구성

--- App.tsx
+++ App.tsx
@@ -5,16 +5,16 @@
  * @format
  */
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import { StatusBar, StyleSheet, useColorScheme, View } from "react-native";
+import WebView from "react-native-webview";
 
 function App() {
-  const isDarkMode = useColorScheme() === 'dark';
+  const isDarkMode = useColorScheme() === "dark";
 
   return (
     <View style={styles.container}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <NewAppScreen templateFileName="App.tsx" />
+      <StatusBar barStyle={isDarkMode ? "light-content" : "dark-content"} />
+      <WebView source={{ uri: "https://www.ycseng.com" }} />
     </View>
   );
 }
