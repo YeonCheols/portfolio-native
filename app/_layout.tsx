6641e8d3138ea4d9927e9ad50706dfaa7bbfd236
SungStack <s9292909@gmail.com>
2025-06-26 10:06:16 +0900

    웹뷰 화면 구성

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,24 +1,51 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import 'react-native-reanimated';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+} from "@react-navigation/native";
+import { useFonts } from "expo-font";
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import "react-native-reanimated";
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { useColorScheme } from "@/hooks/useColorScheme";
+import { WebView } from "react-native-webview";
+import { Platform } from "react-native";
+import { useState } from "react";
 
 export default function RootLayout() {
+  const [ready, setReady] = useState(false);
+  const [webkey, setWebkey] = useState(0);
+
   const colorScheme = useColorScheme();
   const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   });
 
   if (!loaded) {
-    // Async font loading only occurs in development.
     return null;
   }
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+      {Platform.OS !== "web" && (
+        <WebView
+          source={{ uri: "https://www.ycseng.com " }}
+          key={webkey}
+          onLoadStart={(e) => {
+            const { nativeEvent } = e;
+            if (nativeEvent.url === "about:blank" && !ready) {
+              setWebkey(Date.now());
+            }
+          }}
+          onLoadEnd={() => {
+            if (!ready) {
+              setWebkey(Date.now());
+              setReady(true);
+            }
+          }}
+        />
+      )}
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
