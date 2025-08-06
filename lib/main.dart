1609eaa04079e10592cd2c7512359bdc04522559
SungStack <s9292909@gmail.com>
2025-08-07 08:30:38 +0900

    macos, ios 실행 관련 설정파일 크로스플랫폼 호한성 수정

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'dart:html' as html;
 
 void main() {
   runApp(const PortfolioApp());
